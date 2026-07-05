3105dea264e16193b2dd68540408dfa7817a01db
Yassmin-Reda <yassminreda59@gmail.com>
2026-07-05 15:33:17 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/PORTFOLIO_CLIENT/", 
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
-    target: 'es2020',
+    target: "es2020",
     sourcemap: false,
   },
-})
+});
